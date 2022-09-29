ef9a3fd92612b6b4357687dd8aa4b968dad02a4c
Robert Marder <31565706+rmarder@users.noreply.github.com>
2022-09-29 14:17:43 -0500

    add base64 stuff

--- ramnet.hpp
+++ ramnet.hpp
@@ -64,12 +64,22 @@ std::string implode(const std::string &separator, const std::vector<std::string>
 std::string __gethostbyname(const std::string &input);
 std::string url_get_contents(const std::string &input);
 
+// base64 functions
+std::string __base64_decode(const std::string &str);
+std::string __base64_encode(const std::string &str);
+std::string __base64_encode_url(const std::string &str);
+
 // here we redefine functions that collide with the global C namespace used inside the library
 // the library can't see these definitions, this is only for the benefit of library users.
 #ifndef _RAMNET_C_
 
 // network functions
 constexpr auto& gethostbyname = ramnet::__gethostbyname;
+
+// base64 functions
+constexpr auto& base64_decode = ramnet::__base64_decode;
+constexpr auto& base64_encode = ramnet::__base64_encode;
+constexpr auto& base64_encode_url = ramnet::__base64_encode_url;
 
 #endif
 
