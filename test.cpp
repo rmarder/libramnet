65e14d51d891fbc71895ac299e76b549d4f9a5ae
Robert Marder <31565706+rmarder@users.noreply.github.com>
2022-09-29 08:13:44 -0500

    add gethostbyname test, fix formatting

--- test.cpp
+++ test.cpp
@@ -7,14 +7,13 @@ using namespace ramnet;
 // tests for trim, ltrim, rtrim
 void test_trim()
 {
-	// trim single character
 	std::cout << "Tesing trim() ltrim() rtrim() single space...";
 	assert(trim(" test") == "test");
 	assert(trim("test ") == "test");
 	assert(trim(" test ") == "test");
 	assert(ltrim(" test") == "test");
 	assert(rtrim("test ") == "test");
-	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+	std::cout << "\t\t\t[\033[1;32mPASSED\033[0m]" << std::endl;
 
 	std::cout << "Tesing trim() ltrim() rtrim() multiple spaces...";
 	assert(trim("  test") == "test");
@@ -22,7 +21,7 @@ void test_trim()
 	assert(trim("  test  ") == "test");
 	assert(ltrim("  test") == "test");
 	assert(rtrim("test  ") == "test");
-	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+	std::cout << "\t\t[\033[1;32mPASSED\033[0m]" << std::endl;
 
 	std::cout << "Tesing trim() ltrim() rtrim() middle spaces...";
 	assert(trim("  t t") == "t t");
@@ -30,12 +29,12 @@ void test_trim()
 	assert(trim("  t t  ") == "t t");
 	assert(ltrim("  t t") == "t t");
 	assert(rtrim("t t  ") == "t t");
-	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+	std::cout << "\t\t\t[\033[1;32mPASSED\033[0m]" << std::endl;
 
 	std::cout << "Tesing ltrim() rtrim() spaces at wrong end...";
 	assert(rtrim("  t t  ") == "  t t");
 	assert(ltrim("  t t  ") == "t t  ");
-	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+	std::cout << "\t\t\t[\033[1;32mPASSED\033[0m]" << std::endl;
 
 	std::cout << "Tesing trim() ltrim() rtrim() mixed whitespace...";
 	assert(trim("\n\r\t t t") == "t t");
@@ -43,7 +42,7 @@ void test_trim()
 	assert(trim("\r\n\v\t t t \r\n\v\t") == "t t");
 	assert(ltrim("\r\n\v\t t t \r\n\v\t") == "t t \r\n\v\t");
 	assert(rtrim("\r\n\v\t t t \r\n\v\t") == "\r\n\v\t t t");
-	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+	std::cout << "\t\t[\033[1;32mPASSED\033[0m]" << std::endl;
 
 	std::cout << "Tesing trim() ltrim() rtrim() custom characters...";
 	assert(trim("  this is a test  ", "is") == "  this is a test  ");
@@ -51,11 +50,19 @@ void test_trim()
 	assert(trim(" this is a test ", " t") == "his is a tes");
 	assert(trim("  this is a test  ", "ish ") == "this is a test");
 	assert(trim("  this is a test  ", "isht ") == "a te");
-	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+	std::cout << "\t\t[\033[1;32mPASSED\033[0m]" << std::endl;
+}
+
+void test_net()
+{
+	std::cout << "Tesing gethostbyname() on localhost...";
+	assert(gethostbyname("localhost") != "localhost");
+	std::cout << "\t\t\t\t[\033[1;32mPASSED\033[0m]" << std::endl;
 }
 
 int main(void)
 {
 	test_trim();
+	test_net();
 	return 0;
 }
