3e8e5fdaa3aee0e0d1fd57fab56bcfda08ff5dee
Robert Marder <31565706+rmarder@users.noreply.github.com>
2022-09-29 00:27:30 -0500

    add trim tests

--- test.cpp
+++ test.cpp
@@ -1,6 +1,61 @@
 #include "ramnet.hpp"
+#include <cassert>
+#include <iostream>
+
+using namespace ramnet;
+
+// tests for trim, ltrim, rtrim
+void test_trim()
+{
+	// trim single character
+	std::cout << "Tesing trim() ltrim() rtrim() single space...";
+	assert(trim(" test") == "test");
+	assert(trim("test ") == "test");
+	assert(trim(" test ") == "test");
+	assert(ltrim(" test") == "test");
+	assert(rtrim("test ") == "test");
+	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+
+	std::cout << "Tesing trim() ltrim() rtrim() multiple spaces...";
+	assert(trim("  test") == "test");
+	assert(trim("test  ") == "test");
+	assert(trim("  test  ") == "test");
+	assert(ltrim("  test") == "test");
+	assert(rtrim("test  ") == "test");
+	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+
+	std::cout << "Tesing trim() ltrim() rtrim() middle spaces...";
+	assert(trim("  t t") == "t t");
+	assert(trim("t t  ") == "t t");
+	assert(trim("  t t  ") == "t t");
+	assert(ltrim("  t t") == "t t");
+	assert(rtrim("t t  ") == "t t");
+	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+
+	std::cout << "Tesing ltrim() rtrim() spaces at wrong end...";
+	assert(rtrim("  t t  ") == "  t t");
+	assert(ltrim("  t t  ") == "t t  ");
+	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+
+	std::cout << "Tesing trim() ltrim() rtrim() mixed whitespace...";
+	assert(trim("\n\r\t t t") == "t t");
+	assert(trim("t t \v\r\n\t") == "t t");
+	assert(trim("\r\n\v\t t t \r\n\v\t") == "t t");
+	assert(ltrim("\r\n\v\t t t \r\n\v\t") == "t t \r\n\v\t");
+	assert(rtrim("\r\n\v\t t t \r\n\v\t") == "\r\n\v\t t t");
+	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+
+	std::cout << "Tesing trim() ltrim() rtrim() custom characters...";
+	assert(trim("  this is a test  ", "is") == "  this is a test  ");
+	assert(trim(" this is a test ", "t") == " this is a test ");
+	assert(trim(" this is a test ", " t") == "his is a tes");
+	assert(trim("  this is a test  ", "ish ") == "this is a test");
+	assert(trim("  this is a test  ", "isht ") == "a te");
+	std::cout << "[\033[1;32mPASSED\033[0m]" << std::endl;
+}
 
 int main(void)
 {
+	test_trim();
 	return 0;
 }
